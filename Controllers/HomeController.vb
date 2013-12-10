69c07b1d03bc9994a2c7afd06b542732b981a4fd
darksummenors <breakdegydo@hotmail.com>
2013-12-10 12:36:56 +0100

    Version 0.1
    First version of project Etile Modele Bureau

---
+++ Controllers/HomeController.vb
@@ -0,0 +1,27 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "Modify this template to jump-start your ASP.NET MVC application."
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Your app description page."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Your contact page."
+
+        Return View()
+    End Function
+
+    Function test() As ActionResult
+        ViewData("Message") = "Your test homo page."
+
+        Return View()
+    End Function
+End Class
---
+++ Views/Home/test.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+test
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+<h2>test bastardsss</h2>
+    <p></p>
+
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="FeaturedContent" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content4" ContentPlaceHolderID="ScriptsSection" runat="server">
+</asp:Content>
